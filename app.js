93bd6ec4fe1ce08ef4e117a70c7e915d3f959f8c
Eduard Ivankovich <eduard.ivankovich@yandex.ru>
2023-11-15 21:22:50 +0100

    ПР 14

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const checkAuth = require('../middlewares/auth');
+const checkAuth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
