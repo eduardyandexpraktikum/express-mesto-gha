fae00c4c64dbb375f5fe34fc434d758e19ec390c
Eduard Ivankovich <eduard.ivankovich@yandex.ru>
2023-11-19 21:25:48 +0100

    ПР 14

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// const { signInValidation } = require('./middlewares/validator');
+const { signInValidation } = require('./middlewares/validator');
+const { errors } = require('celebrate');
 
 const { PORT = 3000 } = process.env;
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
@@ -17,8 +18,8 @@ const usersRouter = require('./routes/users');
 
 app.use(json());
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', signInValidation, login);
+app.post('/signup', signInValidation, createUser);
 
 app.use(checkAuth);
 
@@ -28,6 +29,14 @@ app.use('/', (req, res, next) => {
   next(res.status(404).send({
     message: 'Невозможно отобразить страницу',
   }));
+});
+
+app.use(errors());
+app.use((err, req, res, next) => {
+  const { statusCode = ServerError } = err;
+  const message = statusCode === ServerError ? 'На сервере произошла ошибка.' : err.message;
+  res.status(statusCode).send({ message });
+  next();
 });
 
 app.listen(PORT);
